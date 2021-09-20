d260342ddb6ddf312b0641a4d6af675d530e0f3a
ponta <coshun@gmail.com>
2021-09-21 03:11:26 +0900

    ping pong test.

--- index.js
+++ index.js
@@ -5,11 +5,30 @@ http.createServer((_, res) => {
     res.end()
 }).listen(8080)
 
+
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+const sendPong = msg => {
+    msg.reply('pong')
+}
+
+const parseMessage = msg => {
+    if (msg.content.startsWith('ping')) sendPong(msg)
+}
+
 client.on('ready', () => {
     console.log('Bot ready.')
+})
+
+client.on('message', msg => {
+    // if (msg.author.bot) return
+    if (msg.author.id === Discord.ClientUser.id) {
+        console.log("my message.")
+        return
+    }
+
+    parseMessage(msg)
 })
 
 client.login(process.env.TOKEN)
