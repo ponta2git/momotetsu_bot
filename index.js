42f9358905879638ac6a8d65a7b7ce5b058ab2fc
ponta <coshun@gmail.com>
2021-09-21 02:10:26 +0900

    base discord login.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hello!');
+import express from 'express'
+import Discord from 'discord.js'
+
+const svr = express()
+const portNo = 8080
+
+svr.get('/', (_, res) => {
+    res.send('alive!')
+})
+
+svr.listen(portNo)
+
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log('Bot ready.')
+})
+
+client.login(process.env.TOKEN)
